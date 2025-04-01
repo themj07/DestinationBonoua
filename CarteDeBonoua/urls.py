a58665b5feb728af11758dbf4a378e546c4324b2
themj07 <amoujeanmichel17@gmail.com>
2025-04-01 15:10:58 +0000

    pour deployer

--- CarteDeBonoua/urls.py
+++ CarteDeBonoua/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('', include("place.urls")),
     path('blog/', include("blog.urls")),
     path('user/', include("user.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
